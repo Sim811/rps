f25ebbd88af1c09b9c5e74e4045b2d73c3702640
Sim <skmatagi@hotmail.com>
2019-09-03 17:17:24 -0600

    latest commit

--- index.js
+++ index.js
@@ -1,35 +1,65 @@
 var rock = document.getElementById("rock");
 var paper = document.getElementById("paper");
 var scissors = document.getElementById("scissors");
+var output = document.getElementById("output")
 var userChoice;
 var compChoice;
+
 var rps = ["rock", "paper", "scissors"];
-var randomNum = Math.floor(Math.random() * 3);
+var array = Math.floor(Math.random() * rps.length);
 
 
-rock.addEventListener("click", function() {
+rock.addEventListener("click", function()  {
+  
   userChoice = this.id;
   // get comp choice
-  compChoice = rps[randomNum];
-  // display those choices to the browser
+  compChoice = rps[array];
   
+  // display those choices to the browser
+  console.log(userChoice, compChoice)
+
   // decide who wins
+  winLose(userChoice, compChoice)
 })
 
 paper.addEventListener("click", function() {
   userChoice = this.id;
   // get comp choice
-  compChoice = rps[randomNum];
+  compChoice = rps[array];
 
   // display those choices to the browser
+  console.log(userChoice, compChoice)
+
   // decide who wins
+  winLose(userChoice, compChoice)
 })
 
 scissors.addEventListener("click", function() {
   userChoice = this.id;
   // get comp choice
-  compChoice = rps[randomNum];
+  compChoice = rps[array];
 
-  // display those choices to the browser
+  // display those choices to the broconsole.log(userChoice, compChoice)
+  
   // decide who wins
+  winLose(userChoice, compChoice)
 })
+
+function winLose(userChoice, compChoice)  {
+  if (userChoice === compChoice){
+  console.log("Tie!");
+  }else if (userChoice === 'paper' && compChoice === 'rock'){
+  console.log("You win!");
+  }else if (userChoice === 'rock' && compChoice === 'scissors'){
+  console.log("You win!");
+  }else if (userChoice === 'scissors' && compChoice === 'rock'){
+  console.log("You lose!");
+  }else if (userChoice === 'rock' && compChoice === 'paper'){
+  console.log("You lose!");
+  }else if (userChoice === 'paper' && compChoice === 'scissors'){
+  console.log("You lose!");
+  }else if (userChoice === 'scissors' || compChoice === 'paper'){
+  console.log("You win!");
+  }
+
+}
